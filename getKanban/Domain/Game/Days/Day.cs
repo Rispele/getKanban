d76b289498b7d84d069a0b85b3306a0f6ee66470
d.smirnov <d.smirnov@kontur.ru>
2024-10-30 02:21:41 +0500

    refactoring: mappings

--- getKanban/Domain/Game/Days/Day.cs
+++ getKanban/Domain/Game/Days/Day.cs
@@ -36,7 +36,7 @@ public class Day
 
 		var diceRoller = new DiceRoller(new Random());
 		var diceNumber = diceRoller.RollDice();
-		var diceScores = MapAnotherTeam(diceNumber);
+		var diceScores = MapDiceNumberToScoreSettings.MapAnotherTeam(diceNumber);
 
 		WorkAnotherTeamDayEvent.CreateInstance(dayContext, diceNumber, diceScores);
 
@@ -78,7 +78,7 @@ public class Day
 			{
 				var asRole = i < swaps.Length ? swaps[i] : role;
 				diceNumber[i] = diceRoller.RollDice();
-				diceScores[i] = MapAnalyst(diceNumber[i], asRole);
+				diceScores[i] = MapDiceNumberToScoreSettings.MapByRole(role, asRole, diceNumber[i]);
 			}
 
 			return (diceNumber, diceScores);
@@ -135,157 +135,4 @@ public class Day
 
 		throw new DayEventNotAwaitedException($"{eventType} is not awaited");
 	}
-
-	#region Mappings
-
-#pragma warning disable CS8509
-	private static int MapAnalyst(int diceNumber, TeamRole asRole)
-	{
-		ValidateDiceNumber(diceNumber);
-
-		switch (asRole)
-		{
-			case TeamRole.Analyst:
-				return diceNumber switch
-				{
-					1 => 3,
-					2 => 4,
-					3 => 4,
-					4 => 5,
-					5 => 5,
-					6 => 6
-				};
-			case TeamRole.Programmer:
-				return diceNumber switch
-				{
-					1 => 1,
-					2 => 2,
-					3 => 0,
-					4 => 2,
-					5 => 1,
-					6 => 3
-				};
-			case TeamRole.Tester:
-				return diceNumber switch
-				{
-					1 => 2,
-					2 => 1,
-					3 => 2,
-					4 => 3,
-					5 => 3,
-					6 => 1
-				};
-			default:
-				throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role");
-		}
-	}
-
-	private static int MapProgrammer(int diceNumber, TeamRole asRole)
-	{
-		ValidateDiceNumber(diceNumber);
-
-		switch (asRole)
-		{
-			case TeamRole.Analyst:
-				return diceNumber switch
-				{
-					1 => 2,
-					2 => 2,
-					3 => 1,
-					4 => 1,
-					5 => 3,
-					6 => 1
-				};
-			case TeamRole.Programmer:
-				return diceNumber switch
-				{
-					1 => 3,
-					2 => 3,
-					3 => 4,
-					4 => 4,
-					5 => 5,
-					6 => 6
-				};
-			case TeamRole.Tester:
-				return diceNumber switch
-				{
-					1 => 1,
-					2 => 1,
-					3 => 2,
-					4 => 0,
-					5 => 2,
-					6 => 3
-				};
-			default:
-				throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role");
-		}
-	}
-
-	private static int MapTester(int diceNumber, TeamRole asRole)
-	{
-		ValidateDiceNumber(diceNumber);
-
-		switch (asRole)
-		{
-			case TeamRole.Analyst:
-				return diceNumber switch
-				{
-					1 => 1,
-					2 => 0,
-					3 => 2,
-					4 => 1,
-					5 => 2,
-					6 => 3
-				};
-			case TeamRole.Programmer:
-				return diceNumber switch
-				{
-					1 => 2,
-					2 => 1,
-					3 => 1,
-					4 => 2,
-					5 => 3,
-					6 => 1
-				};
-			case TeamRole.Tester:
-				return diceNumber switch
-				{
-					1 => 3,
-					2 => 4,
-					3 => 4,
-					4 => 5,
-					5 => 5,
-					6 => 6
-				};
-			default:
-				throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role");
-		}
-	}
-
-	private static int MapAnotherTeam(int diceNumber)
-	{
-		ValidateDiceNumber(diceNumber);
-
-		return diceNumber switch
-		{
-			1 => 0,
-			2 => 2,
-			3 => 3,
-			4 => 4,
-			5 => 5,
-			6 => 15
-		};
-	}
-
-	private static void ValidateDiceNumber(int diceNumber)
-	{
-		if (diceNumber is < 1 or > 6)
-		{
-			throw new InvalidOperationException(
-				$"Invalid {nameof(diceNumber)}:{diceNumber}. Should be between 1 and 6");
-		}
-	}
-#pragma warning restore CS8509
-
-	#endregion
 }
---
+++ getKanban/Domain/Game/Days/MapDiceNumberToScoreSettings.cs
@@ -0,0 +1,149 @@
+﻿using Domain.Game.Days.DayEvents.UpdateTeamRolesDayEvent;
+
+#pragma warning disable CS8509 // The switch expression does not handle all possible values of its input type (it is not exhaustive).
+
+namespace Domain.Game;
+
+public class MapDiceNumberToScoreSettings
+{
+	public static int MapByRole(TeamRole role, TeamRole asRole, int diceNumber)
+	{
+		ValidateDiceNumber(diceNumber);
+		return role switch
+		{
+			TeamRole.Analyst => MapAnalyst(diceNumber, asRole),
+			TeamRole.Programmer => MapProgrammer(diceNumber, asRole),
+			TeamRole.Tester => MapTester(diceNumber, asRole),
+			_ => throw new ArgumentOutOfRangeException(nameof(role), role, "Invalid team role")
+		};
+	}
+
+	public static int MapAnotherTeam(int diceNumber)
+	{
+		ValidateDiceNumber(diceNumber);
+
+		return diceNumber switch
+		{
+			1 => 0,
+			2 => 2,
+			3 => 3,
+			4 => 4,
+			5 => 5,
+			6 => 15
+		};
+	}
+
+	private static int MapAnalyst(int diceNumber, TeamRole asRole)
+	{
+		return asRole switch
+		{
+			TeamRole.Analyst => diceNumber switch
+			{
+				1 => 3,
+				2 => 4,
+				3 => 4,
+				4 => 5,
+				5 => 5,
+				6 => 6
+			},
+			TeamRole.Programmer => diceNumber switch
+			{
+				1 => 1,
+				2 => 2,
+				3 => 0,
+				4 => 2,
+				5 => 1,
+				6 => 3
+			},
+			TeamRole.Tester => diceNumber switch
+			{
+				1 => 2,
+				2 => 1,
+				3 => 2,
+				4 => 3,
+				5 => 3,
+				6 => 1
+			},
+			_ => throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role")
+		};
+	}
+
+	private static int MapProgrammer(int diceNumber, TeamRole asRole)
+	{
+		return asRole switch
+		{
+			TeamRole.Analyst => diceNumber switch
+			{
+				1 => 2,
+				2 => 2,
+				3 => 1,
+				4 => 1,
+				5 => 3,
+				6 => 1
+			},
+			TeamRole.Programmer => diceNumber switch
+			{
+				1 => 3,
+				2 => 3,
+				3 => 4,
+				4 => 4,
+				5 => 5,
+				6 => 6
+			},
+			TeamRole.Tester => diceNumber switch
+			{
+				1 => 1,
+				2 => 1,
+				3 => 2,
+				4 => 0,
+				5 => 2,
+				6 => 3
+			},
+			_ => throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role")
+		};
+	}
+
+	private static int MapTester(int diceNumber, TeamRole asRole)
+	{
+		return asRole switch
+		{
+			TeamRole.Analyst => diceNumber switch
+			{
+				1 => 1,
+				2 => 0,
+				3 => 2,
+				4 => 1,
+				5 => 2,
+				6 => 3
+			},
+			TeamRole.Programmer => diceNumber switch
+			{
+				1 => 2,
+				2 => 1,
+				3 => 1,
+				4 => 2,
+				5 => 3,
+				6 => 1
+			},
+			TeamRole.Tester => diceNumber switch
+			{
+				1 => 3,
+				2 => 4,
+				3 => 4,
+				4 => 5,
+				5 => 5,
+				6 => 6
+			},
+			_ => throw new ArgumentOutOfRangeException(nameof(asRole), asRole, "Unknown role")
+		};
+	}
+
+	private static void ValidateDiceNumber(int diceNumber)
+	{
+		if (diceNumber is < 1 or > 6)
+		{
+			throw new InvalidOperationException(
+				$"Invalid {nameof(diceNumber)}:{diceNumber}. Should be between 1 and 6");
+		}
+	}
+}
