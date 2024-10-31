ce66bd3b866131e09443bc2ba9e32393a2bda858
Vearsmon <vearsmon285537@gmail.com>
2024-10-31 16:52:33 +0500

    gkn-9: feat: rework with controllers

---
+++ getKanban/Web/Controllers/TeamsCreationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+public class TeamsCreationController : Controller
+{
+	public TeamsCreationController()
+	{
+		
+	}
+	
+	public IActionResult Menu()
+	{
+		return View();
+	}
+}
--- getKanban/Web/Program.cs
+++ getKanban/Web/Program.cs
@@ -1,38 +1,28 @@
-using Domain.Game;
-using Microsoft.AspNetCore.Mvc;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddTransient<HtmlPageHandler>();
+
+// builder.Services.AddTransient<HtmlPageHandler>();
+
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
 
 var app = builder.Build();
 
-app.MapControllers();
+var loggerFactory = app.Services.GetService<ILoggerFactory>();
 
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/get", async (context) =>
-{
-	await context.Response.WriteAsync(app.Services.GetRequiredService<HtmlPageHandler>().GetHtmlPageContent());
-});
-app.MapGet("/roll", () => new DiceRoller(new Random()).RollDice());
-app.MapGet("/add/{count}", (int count) =>
-{
-	var result = "";
-	for (int i = 0; i < count; i++)
-	{
-		result += "Team " + (i + 1) + "\n";
-	}
-	return result;
-});
+app.MapControllers();
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}");
 
 app.Run();
-
-public class HtmlPageHandler
-{
-	public string GetHtmlPageContent()
-	{
-		return File.ReadAllText("Pages/index.html");
-	}
-}
