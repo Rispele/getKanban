a85b3c6527c83089b3e429c8ec0c99647cc368de
Vearsmon <vearsmon285537@gmail.com>
2024-11-06 13:56:40 +0500

    feat: joining game implementation

---
+++ getKanban/Web/Controllers/GameLobbyController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+public class GameLobbyController : Controller
+{
+
+	[HttpGet]
+	[Route("lobby")]
+	public IActionResult Lobby(Guid id)
+	{
+		//var teamsData = ...;
+		return View(id);
+	}
+}
---
+++ getKanban/Web/Controllers/JoinGameController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.Controllers;
+
+[Route("join")]
+public class JoinGameController : Controller
+{
+	[HttpGet]
+	[Route("game")]
+	public IActionResult JoinGame()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	[Route("game")]
+	public bool CheckForOpenedGame(Guid id)
+	{
+		return true;
+	}
+}
