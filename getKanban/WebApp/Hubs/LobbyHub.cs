da844a73849eea902cb4fd462e42c6c9733444c9
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2024-11-07 02:44:37 +0500

    feat: add some hubs

---
+++ getKanban/WebApp/Hubs/LobbyHub.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace WebApp.Hubs;
+
+public class LobbyHub : Hub
+{
+	public async Task Join(Guid gameSessionId, Guid? teamId)
+	{
+		var userId = GetUserIdOrThrow();
+
+		//TODO: валидируем что пользователь действительно есть в комманде или в ангелах
+
+		var groupId = GetGroupId(gameSessionId);
+		await Groups.AddToGroupAsync(Context.ConnectionId, groupId);
+		await Clients.OthersInGroup(groupId).SendCoreAsync("NotifyJoined", [teamId, userId]);
+	}
+
+	public async Task StartGame(Guid gameSessionId)
+	{
+		var userId = GetUserIdOrThrow();
+		
+		//TODO: проверяем, что пользователь - создатель игры
+	
+		var groupId = GetGroupId(gameSessionId);
+		await Clients.Group(groupId).SendAsync("NotifyStarted");
+	}
+
+	private string GetUserIdOrThrow() =>
+		Context.GetHttpContext()?.Request.Cookies["userId"] ?? throw new NullReferenceException();
+
+	private static string GetGroupId(Guid gameSessionId) => $"lobby-{gameSessionId}";
+}
