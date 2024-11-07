4a8e82cadeaef2fbcd9b77b0e321511798809b33
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2024-11-08 01:12:46 +0500

    fix: json serializer

--- getKanban/Domain/Serializers/SerializerExtensions.cs
+++ getKanban/Domain/Serializers/SerializerExtensions.cs
@@ -6,7 +6,7 @@ public static class SerializerExtensions
 {
 	private static readonly JsonSerializerSettings Settings = new()
 	{
-		TypeNameHandling = TypeNameHandling.Auto
+`		TypeNameHandling = TypeNameHandling.Objects
 	};
 
 	public static string ToJson<T>(this T value)
