2b83614f69c1672a01af15b25f42c1108175f19b
Vearsmon <vearsmon285537@gmail.com>
2024-11-27 20:40:39 +0500

    feat: ticket and role update

--- getKanban/WebApp/Controllers/DayStepsController.cs
+++ getKanban/WebApp/Controllers/DayStepsController.cs
@@ -10,13 +10,13 @@ public class DayStepsController : Controller
 {
 	private readonly IGameSessionService gameSessionService;
 	private readonly ITeamService teamService;
-	
+
 	public DayStepsController(IGameSessionService gameSessionService, ITeamService teamService)
 	{
 		this.gameSessionService = gameSessionService;
 		this.teamService = teamService;
 	}
-	
+
 	[HttpGet]
 	[Route("1")]
 	[Route("1/0")]
@@ -24,14 +24,14 @@ public class DayStepsController : Controller
 	{
 		return View(await gameSessionService.GetCurrentSessionId(RequestContextFactory.Build(Request)));
 	}
-	
+
 	[HttpGet]
 	[Route("1/1")]
 	public IActionResult Step1Stage1()
 	{
 		return View();
 	}
-	
+
 	[HttpGet]
 	[Route("1/2")]
 	public IActionResult Step1Stage2()
@@ -41,32 +41,29 @@ public class DayStepsController : Controller
 
 	[HttpPost]
 	[Route("save-roles-transformation")]
-	public async Task SaveRolesTransformation([FromBody] string[][] transformations)
+	public async Task SaveRolesTransformation([FromBody] string[] transformation)
 	{
 		var currentSessionId = await gameSessionService.GetCurrentSessionId(RequestContextFactory.Build(Request));
 		var currentUser = await gameSessionService.GetCurrentUser(RequestContextFactory.Build(Request));
 		var currentTeam = await gameSessionService.GetCurrentTeam(
 			RequestContextFactory.Build(Request),
 			currentSessionId!.Value);
-		
-		foreach (var transformation in transformations)
-		{
-			var roleFrom = Enum.Parse<TeamRole>(transformation[0]);
-			var roleTo = Enum.Parse<TeamRole>(transformation[1]);
-			
-			await teamService.PatchDayAsync(
-				currentSessionId!.Value,
-				currentTeam!.Id,
-				currentUser.Id,
-				new UpdateTeamRolesCommand
-				{
-					From = roleFrom,
-					To = roleTo,
-					Remove = false
-				});
-		}
+
+		var roleFrom = Enum.Parse<TeamRole>(transformation[0]);
+		var roleTo = Enum.Parse<TeamRole>(transformation[1]);
+
+		await teamService.PatchDayAsync(
+			currentSessionId!.Value,
+			currentTeam!.Id,
+			currentUser.Id,
+			new UpdateTeamRolesCommand
+			{
+				From = roleFrom,
+				To = roleTo,
+				Remove = false
+			});
 	}
-	
+
 	[HttpGet]
 	[Route("2")]
 	[Route("2/0")]
@@ -77,7 +74,7 @@ public class DayStepsController : Controller
 		var currentTeam = await gameSessionService.GetCurrentTeam(
 			RequestContextFactory.Build(Request),
 			currentSessionId!.Value);
-		
+
 		var diceRollResult = await teamService.GetCurrentDayAsync(currentSessionId!.Value, currentTeam!.Id);
 		return View(diceRollResult.RollDiceContainer);
 	}
@@ -99,7 +96,7 @@ public class DayStepsController : Controller
 			new RollDiceCommand()
 		);
 	}
-	
+
 	[HttpGet]
 	[Route("3")]
 	[Route("3/0")]
@@ -107,7 +104,7 @@ public class DayStepsController : Controller
 	{
 		return View();
 	}
-	
+
 	[HttpGet]
 	[Route("4")]
 	[Route("4/0")]
@@ -115,21 +112,57 @@ public class DayStepsController : Controller
 	{
 		return View();
 	}
-	
+
 	[HttpGet]
 	[Route("4/1")]
 	public IActionResult Step4Stage1()
 	{
 		return View();
 	}
-	
+
+	public class TicketModel
+	{
+		public string TicketId { get; set; }
+		public bool Remove { get; set; }
+	}
+
+	[HttpPost]
+	[Route("update-sprint-backlog")]
+	public async Task UpdateSprintBacklog([FromBody] TicketModel ticketModel)
+	{
+		var currentSessionId = await gameSessionService.GetCurrentSessionId(RequestContextFactory.Build(Request));
+		var currentUser = await gameSessionService.GetCurrentUser(RequestContextFactory.Build(Request));
+		var currentTeam = await gameSessionService.GetCurrentTeam(
+			RequestContextFactory.Build(Request),
+			currentSessionId!.Value);
+		
+		await teamService.PatchDayAsync(
+			currentSessionId!.Value,
+			currentTeam!.Id,
+			currentUser.Id,
+			new UpdateSprintBacklogCommand()
+			{
+				TicketIds = new[] { ticketModel.TicketId },
+				Remove = ticketModel.Remove
+			}
+		);
+	}
+
 	[HttpGet]
 	[Route("4/2")]
-	public IActionResult Step4Stage2()
+	public async Task<IActionResult> Step4Stage2()
 	{
-		return View();
+		var currentSessionId = await gameSessionService.GetCurrentSessionId(RequestContextFactory.Build(Request));
+		var currentUser = await gameSessionService.GetCurrentUser(RequestContextFactory.Build(Request));
+		var currentTeam = await gameSessionService.GetCurrentTeam(
+			RequestContextFactory.Build(Request),
+			currentSessionId!.Value);
+		var ticketIds = (await teamService.GetCurrentDayAsync(currentSessionId!.Value, currentTeam!.Id))
+			.UpdateSprintBacklogContainer.TicketIds.ToList();
+		
+		return View(ticketIds);
 	}
-	
+
 	[HttpGet]
 	[Route("5")]
 	[Route("5/0")]
@@ -137,14 +170,14 @@ public class DayStepsController : Controller
 	{
 		return View();
 	}
-	
+
 	[HttpGet]
 	[Route("5/1")]
 	public IActionResult Step5Stage1()
 	{
 		return View();
 	}
-	
+
 	[HttpGet]
 	[Route("5/2")]
 	public IActionResult Step5Stage2()
