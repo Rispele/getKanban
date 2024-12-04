26906075fcd6a37ffc3a2b538da108d0bfc45cdc
Dmitriy Smirnov <d.smirnov@skbkontur.ru>
2024-12-05 01:00:28 +0500

    feat: add css for: - Step 2 page (step2.css) - Angel's pages (helpers.css)
    add html templates for:
    - Step 4 stage 1 (in dir DaySteps/Step4Versions)
    - Step 5 stage 1 (same as s4s1)
    - Step 6 stage 1
    
    fix:
    css for:
    - Step 4 Stage 1 (checkboxPages.css)
    - Step 5 Stage 1 (checkboxPages.css)
    
    html for:
    - Step 2 Stage 0
    - Cards in Step 4 Stage 2 and Step 5 Stage 2

---
+++ getKanban/WebApp/Views/DaySteps/Step4Versions/Step4Stage1SingleVersion.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.DaySteps.Step4Versions;
+
+public class Step4Stage1SingleVersion : PageModel
+{
+	public void OnGet()
+	{
+		
+	}
+}
