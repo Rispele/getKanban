4675cc6ab0bf4469f2975375c681017dda7ea6a7
Vearsmon <vearsmon285537@gmail.com>
2024-12-06 18:07:49 +0500

    feat: error styles and script

---
+++ getKanban/WebApp/wwwroot/js/error.js
@@ -0,0 +1,14 @@
+﻿function showError(message) {
+    const errorCard = document.createElement('div');
+    errorCard.className = 'error-card';
+    errorCard.textContent = message;
+
+    document.body.appendChild(errorCard);
+
+    setTimeout(() => {
+        errorCard.style.opacity = '0';
+        setTimeout(() => {
+            document.body.removeChild(errorCard);
+        }, 500);
+    }, 5000);
+}
