6179a96dbd201a88d6c94839256543521ed251f5
d.smirnov <d.smirnov@kontur.ru>
2024-12-24 20:32:38 +0500

    feat: connection restoring

--- getKanban/WebApp/wwwroot/js/setup-hub-connection.js
+++ getKanban/WebApp/wwwroot/js/setup-hub-connection.js
@@ -1,8 +1,14 @@
 ﻿function setupHubConnection(builder, sessionId, teamId, redirect = true) {
-    const connection = builder.withUrl("/teamSessionHub").build();
+    const connection = builder
+        .withUrl("/teamSessionHub")
+        .withAutomaticReconnect([0, 3000, 5000, 10000, 15000, 30000])
+        .build();
+    connection.onreconnected(async function () {
+        window.location.reload();
+    });
     connection.start()
-        .then(function () {
-            connection.invoke("Join", sessionId, teamId);
+        .then(async function () {
+            await connection.invoke("Join", sessionId, teamId);
         });
     if (redirect) {
         connection.on("NotifyPageChange", function (page, stage) {
